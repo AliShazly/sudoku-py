505667355ffd3adfb7cbbc174ba9bf24da82378b
Ali Shazly <alishazly302@gmail.com>
2019-02-22 18:04:22 -0800

    depth first solve works with easy puzzles

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,4 @@
-sudoku = [[5,3,0,0,7,0,0,0,0],
+puzzle_easy = [[5,3,0,0,7,0,0,0,0],
           [6,0,0,1,9,5,0,0,0],
           [0,9,8,0,0,0,0,6,0],
           [8,0,0,0,6,0,0,0,3],
@@ -7,6 +7,16 @@ sudoku = [[5,3,0,0,7,0,0,0,0],
           [0,6,0,0,0,0,2,8,0],
           [0,0,0,4,1,9,0,0,5],
           [0,0,0,0,8,0,0,7,9]]
+
+puzzle_hard = [[0,0,0,5,0,1,7,0,2],
+            [0,0,0,6,0,0,0,3,0],
+            [1,0,0,0,8,0,6,0,4],
+            [5,0,7,0,0,3,0,6,0],
+            [8,0,0,0,0,0,0,0,3],
+            [0,2,0,9,0,0,1,0,8],
+            [9,0,2,0,6,0,0,0,7],
+            [0,8,0,0,0,4,0,0,0],
+            [4,0,3,2,0,7,0,0,0]]
 
 def get_row(puzzle, row_num):
     return puzzle[row_num]
@@ -17,15 +27,13 @@ def get_column(puzzle, col_num):
 def get_square(puzzle, row_num, col_num):
     square_x = row_num // 3
     square_y = col_num // 3
-    # Fuck you stackoverflow
-    # coords = [[(square_x * 3 + j, square_y * 3 + i) for j in range(3)] for i in range(3)]
     coords = []
     for i in range(3):
         for j in range(3):
             coords.append((square_x * 3 + j, square_y * 3 + i))
-    return [sudoku[i[0]][i[1]] for i in coords]
+    return [puzzle[i[0]][i[1]] for i in coords]
 
-def possibilities(puzzle, row_num, col_num):
+def get_possibilities(puzzle, row_num, col_num):
     possible = set(range(1,10))
     row = get_row(puzzle, row_num)
     col = get_column(puzzle, col_num)
@@ -33,4 +41,17 @@ def possibilities(puzzle, row_num, col_num):
     not_possible = set(row + col + square)
     return possible - not_possible
 
-print(possibilities(sudoku, 0, 2))
+def depth_first_solve(puzzle):
+    unsolved = False
+    for row_num, row_values in enumerate(puzzle):
+        for col_num, value in enumerate(row_values):
+            if value == 0:
+                unsolved = True
+                possibilities = list(get_possibilities(puzzle, row_num, col_num))
+                if len(possibilities) == 1:
+                    puzzle[row_num][col_num] = possibilities[0]
+    if unsolved:
+        return depth_first_solve(puzzle)
+    return puzzle
+
+print(depth_first_solve(puzzle_easy))
