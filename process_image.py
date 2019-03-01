8054da2f16bfa23b9a1d2bb902a26be016326207
Ali Shazly <alishazly302@gmail.com>
2019-02-28 17:08:07 -0800

    Corners work correctly

--- process_image.py
+++ process_image.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('assets/puzzle.jpg', 0)
-width, height = img.shape
-img = cv2.GaussianBlur(img, (width, height), 1)
+img_rgb = cv2.imread('assets/img.jpg', cv2.IMREAD_COLOR)
 
 
 def process(img):
     kernel = np.ones((2, 2), np.uint8)
-    thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(greyscale, (7, 7), 0)
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                    cv2.THRESH_BINARY, 11, 2)
     inverted = cv2.bitwise_not(thresh, 0)
     denoise = cv2.morphologyEx(inverted, cv2.MORPH_OPEN, kernel)
@@ -16,9 +16,30 @@ def process(img):
     return dilated
 
 
-processed = process(img)
-contours, hire = cv2.findContours(processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
+def get_corners(img):
+    contours, hire = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+    largest_contour = np.squeeze(contours[0])  # Getting rid of extra dimenstions
 
-processed = cv2.cvtColor(processed, cv2.COLOR_GRAY2RGB)
-biggest_square_draw = cv2.drawContours(processed, [contours[0]], -1, (0, 255, 0), 2)
+    sums = [sum(i) for i in largest_contour]
+    differences = [i[0] - i[1] for i in largest_contour]
+
+    top_left = np.argmin(sums)
+    top_right = np.argmax(differences)
+    bottom_left = np.argmax(sums)
+    bottom_right = np.argmin(differences)
+
+    corners = [largest_contour[top_left], largest_contour[top_right], largest_contour[bottom_left],
+               largest_contour[bottom_right]]
+    return corners
+
+
+processed = process(img_rgb)
+corners = get_corners(processed)
+
+for point in corners:
+    img = cv2.circle(img_rgb, tuple(int(x) for x in point), 5, (0, 0, 255), -1)
+
+cv2.imshow('w', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
